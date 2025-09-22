ee8fe6e8ab8a50db35805c207d00f186ebe24cc0
yaodao <yaodao456@gmail.com>
2025-09-22 16:21:57 +0800

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   let currentDate = new Date();
 
   // Target future date/24 hour time/Timezone
-  let targetDate = moment.tz("2026-05-20 15:00", "Asia/Kolkata");
+  let targetDate = moment.tz("2025-09-21 16:25", "Asia/Kolkata");
 
   // Calculate the difference in seconds between the future and current date
   let diff = targetDate / 1000 - currentDate.getTime() / 1000;
@@ -14,7 +14,7 @@ $(document).ready(function() {
   FlipClock.Lang.Chinese = {
     'years': '年',
     'months': '月',
-    'days': '日',
+    'days': '天',
     'hours': '时',
     'minutes': '分',
     'seconds': '秒'
